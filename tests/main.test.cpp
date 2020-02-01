d36876e3fbfd051bac10bd2c992633fab4c872e7
Aaron Hornby (Laptop) <aaron.hornby@outlook.com>
2020-01-31 22:01:38 -0700

    added main external test runner file, setup easier external testing

---
+++ tests/main.test.cpp
@@ -0,0 +1,13 @@
+//NOTE: this is the main file that runs all linked external tests, which can be filtered (see doctest manual for instructions) to only run specific pattern matched test cases
+// reference (permalink): https://github.com/onqtam/doctest/blob/503de03ee2cbbfc58c2063dce36dc96f917f2da3/doctest/doctest.h#L5931-L5935
+//      #ifdef DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+//      DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007) // 'function' : must be 'attribute' - see issue #182
+//      int main(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
+//      DOCTEST_MSVC_SUPPRESS_WARNING_POP
+//      #endif // DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+namespace prefix {
+
+}
--- tests/utils/string-utils.test.cpp
+++ tests/utils/string-utils.test.cpp
@@ -1,10 +1,3 @@
-// reference (permalink): https://github.com/onqtam/doctest/blob/503de03ee2cbbfc58c2063dce36dc96f917f2da3/doctest/doctest.h#L5931-L5935
-//      #ifdef DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-//      DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007) // 'function' : must be 'attribute' - see issue #182
-//      int main(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
-//      DOCTEST_MSVC_SUPPRESS_WARNING_POP
-//      #endif // DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
 #include <cpp-xplatform/utils/string-utils.hpp>
